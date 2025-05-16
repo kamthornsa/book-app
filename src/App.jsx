cb0769dd0f6ac33f74876820bce01d2d221576e5
kamthorn <kamthorn.sa@ksu.ac.th>
2025-05-16 12:01:33 +0700

    week 1

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div>Sidebar</div>
+      <div>Content</div>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
